5eede7d6cfe70a7f8b3c6c10833ed5323dec4226
Tyler Swayne <swaynetyler@gmail.com>
2019-09-15 16:49:50 -0700

    add bool, lock joi

--- src/config/types.js
+++ src/config/types.js
@@ -4,5 +4,6 @@ module.exports = {
   string: Joi.string,
   enum: (set=[]) => () => Joi.any().valid(...set),
   object: Joi.object,
+  boolean: Joi.boolean,
   arrayEnum: (set=[]) => () => Joi.array().items(Joi.string().valid(...set))
 }
